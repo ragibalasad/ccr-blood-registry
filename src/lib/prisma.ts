eaac78f4f6081b38cc5625c2122e635b4dbe35c4
Ragib Al Asad <ragibalasad@gmail.com>
2026-04-08 04:34:40 +0600

    Fix Prisma + Vercel env issues, remove dotenv, use default client

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,5 +1,5 @@
 import { PrismaNeonHttp } from '@prisma/adapter-neon';
-import { PrismaClient } from '../generated/client';
+import { PrismaClient } from '@prisma/client';
 
 const globalForPrisma = globalThis as unknown as {
   prisma: PrismaClient | undefined;
